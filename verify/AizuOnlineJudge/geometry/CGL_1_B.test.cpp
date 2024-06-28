f7c732f229ddf69e643d4d4a01727d0630148c05
lmorinn <trin123123m@gmail.com>
2024-06-28 14:54:32 +0900

    bug fix

--- verify/AizuOnlineJudge/geometry/CGL_1_B.test.cpp
+++ verify/AizuOnlineJudge/geometry/CGL_1_B.test.cpp
@@ -1,18 +1,17 @@
+#include "../../../template/template.hpp"
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_1_B"
 #define ERROR 1e-8
-#include <bits/stdc++.h>
-using namespace std;
 #include "../../../geometry/Geometry.hpp"
 int main() {
   cin.tie(0)->sync_with_stdio(0);
-  int x1, y1, x2, y2;
-  cin >> x1 >> y1 >> x2 >> y2;
+  int a, b, c, d;
+  in(a, b, c, d);
   Line l(Point(x1, y1), Point(x2, y2));
   int q;
-  cin >> q;
+  in(q);
   for (int i = 0; i < q; i++) {
     int x, y;
-    cin >> x >> y;
+    in(x, y);
     outf(reflect(l, Point(x, y)));
   }
 }
