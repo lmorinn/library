22eeeb7c1f5ac25ed1af5008f72d0b8bc4a92e72
lmorinn <trin123123m@gmail.com>
2024-11-29 03:35:19 +0900

    ダイクストラ法のupdate

--- graph/shortest-path/Dijkstra.hpp
+++ graph/shortest-path/Dijkstra.hpp
@@ -1,27 +1,82 @@
-#pragma once
-
 template <class T>
-vector<T> shortest_dijkstra(vector<vector<pair<T, T>>> &g, T start = 0) {
-  int n = g.size();
-  vector<T> dist(n, numeric_limits<T>::max());
-  vector<bool> fin(n, false);
-  priority_queue<pair<T, T>, vector<pair<T, T>>, greater<pair<T, T>>> q;
-  dist[start] = 0;
-  q.push({0, start});
+class Dijkstra {
+   private:
+    vector<vector<pair<int, T>>> g;
+    vector<T> dist;
+    vector<int> prev;
+    int n;
 
-  while (!q.empty()) {
-    T cur = q.top().second;
-    q.pop();
-    if (fin[cur]) continue;
-    fin[cur] = true;
-    for (auto p : g[cur]) {
-      T nex = p.second;
-      T weight = p.first;
-      if (dist[nex] > dist[cur] + weight) {
-        dist[nex] = dist[cur] + weight;
-        q.push({dist[nex], nex});
-      }
+   public:
+    Dijkstra(vector<vector<pair<int, T>>> _g, int start = 0) {
+        g = _g;
+        n = g.size();
+        dist.assign(n, -1);
+        prev.assign(n, -1);
+        vector<bool> fin(n, false);
+        priority_queue<tuple<T, int, int>, vector<tuple<T, int, int>>, greater<tuple<T, int, int>>> q;
+        dist[start] = 0;
+        q.emplace(0, start, -1);
+        while (!q.empty()) {
+            int cur = get<1>(q.top());
+            int pre = get<2>(q.top());
+            q.pop();
+            if (fin[cur]) continue;
+            fin[cur] = true;
+            prev[cur] = pre;
+            for (auto p : g[cur]) {
+                int nex = p.first;
+                int weight = p.second;
+                if (dist[nex] == -1 or dist[nex] > dist[cur] + weight) {
+                    dist[nex] = dist[cur] + weight;
+                    q.emplace(dist[nex], nex, cur);
+                }
+            }
+        }
     }
-  }
-  return dist;
-}
+
+    void rebuild(int start) {
+        dist.assign(n, -1);
+        prev.assign(n, -1);
+        vector<bool> fin(n, false);
+        priority_queue<tuple<T, int, int>, vector<tuple<T, int, int>>, greater<tuple<T, int, int>>> q;
+        dist[start] = 0;
+        q.emplace(0, start, -1);
+        while (!q.empty()) {
+            int cur = get<1>(q.top());
+            int pre = get<2>(q.top());
+            q.pop();
+            if (fin[cur]) continue;
+            fin[cur] = true;
+            prev[cur] = pre;
+            for (auto p : g[cur]) {
+                int nex = p.first;
+                int weight = p.second;
+                if (dist[nex] == -1 or dist[nex] > dist[cur] + weight) {
+                    dist[nex] = dist[cur] + weight;
+                    q.emplace(dist[nex], nex, cur);
+                }
+            }
+        }
+    }
+
+    vector<T> get_dist() {
+        return dist;
+    }
+
+    T get_dist(int t) {
+        return dist[t];
+    }
+
+    vector<int> get_route(int t) {
+        vector<int> ret;
+        if (dist[t] == -1) return ret;
+
+        int cur = t;
+        while (cur != -1) {
+            ret.emplace_back(cur);
+            cur = prev[cur];
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
--- verify/AizuOnlineJudge/graph/shortest-path/ALDS1_12_C.test.cpp
+++ verify/AizuOnlineJudge/graph/shortest-path/ALDS1_12_C.test.cpp
@@ -5,20 +5,21 @@ using namespace std;
 #include "../../../../graph/shortest-path/Dijkstra.hpp"
 
 int main() {
-  cin.tie(0)->sync_with_stdio(0);
-  int n;
-  cin >> n;
-  vector<vector<pair<long long, long long>>> g(n);
-  for (int i = 0; i < n; i++) {
-    int u, k, c, v;
-    cin >> u >> k;
-    for (int j = 0; j < k; j++) {
-      cin >> v >> c;
-      g[u].push_back({c, v});
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<vector<pair<int, long long>>> g(n);
+    for (int i = 0; i < n; i++) {
+        int u, k, c, v;
+        cin >> u >> k;
+        for (int j = 0; j < k; j++) {
+            cin >> v >> c;
+            g[u].emplace_back(v, c);
+        }
     }
-  }
-  vector<long long> res = shortest_dijkstra<long long>(g);
-  for (int i = 0; i < n; i++) {
-    cout << i << " " << res[i] << "\n";
-  }
+    Dijkstra<long long> d(g);
+    vector<long long> res = d.get_dist();
+    for (int i = 0; i < n; i++) {
+        cout << i << " " << res[i] << "\n";
+    }
 }
