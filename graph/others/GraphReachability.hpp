601ee07b78e8f2070c74907eeaec3eced40f0ceb
lmorinn <trin123123m@gmail.com>
2024-12-07 20:14:42 +0900

    std::bitsetを使用するように変更

--- graph/others/GraphReachability.hpp
+++ graph/others/GraphReachability.hpp
@@ -50,24 +50,24 @@ class GraphReachability {
         vector<bool> res(int(q.size()));
         int res_idx = 0;
         int cnt = 0;
-        vector<unsigned long long> dp(n);
-        vector<int> goal(64);
+        vector<bitset<2048>> dp(n);
+        vector<int> goal(2048);
 
         while (!q.empty()) {
             int a = compress_id[q.front().first];
             int b = compress_id[q.front().second];
             q.pop();
-            dp[a] |= (1ULL << cnt);
+            dp[a].set(cnt);
             goal[cnt] = b;
             cnt++;
-            if (cnt == 64 or q.empty()) {
+            if (cnt == 2048 or q.empty()) {
                 for (int i = 0; i < n; i++) {
                     for (int prev : r[i]) {
                         dp[i] |= dp[prev];
                     }
                 }
                 for (int i = 0; i < cnt; i++) {
-                    if (dp[goal[i]] & (1ULL << i)) {
+                    if (dp[goal[i]][i]) {
                         res[res_idx] = true;
                     } else {
                         res[res_idx] = false;
@@ -76,7 +76,7 @@ class GraphReachability {
                 }
 
                 for (int i = 0; i < n; i++) {
-                    dp[i] = 0;
+                    dp[i].reset();
                 }
                 cnt = 0;
             }
