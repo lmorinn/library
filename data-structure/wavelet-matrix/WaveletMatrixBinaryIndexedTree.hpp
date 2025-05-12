5d4b4f19914547270ec554c3a8b8e94e478dae17
lmorinn <trin123123m@gmail.com>
2025-05-12 11:31:04 +0900

    wm fenwickドキュメント追加に伴う変更

--- data-structure/wavelet-matrix/WaveletMatrixBinaryIndexedTree.hpp
+++ data-structure/wavelet-matrix/WaveletMatrixBinaryIndexedTree.hpp
@@ -1,294 +1,213 @@
-#include "../binary-indexed-tree/BinaryIndexedTree.hpp"
-template <class T>
 struct BitVector {
-    unsigned sz;
-    unsigned blocksize;
-    vector<unsigned long long> bit;
-    vector<unsigned> sum;
-    fenwick_tree<T> seg;
+  unsigned sz;
+  unsigned blocksize;
+  vector<unsigned> bit, sum;
 
-    BitVector() {}
+  BitVector() {}
 
-    BitVector(unsigned siz) {
-        sz = siz;
-        blocksize = (sz + 63) >> 6;
-        bit.assign(blocksize, 0ULL);
-        sum.assign(blocksize, 0U);
+  BitVector(unsigned siz) {
+    sz = siz;
+    blocksize = (sz + 31) >> 5;
+    bit.assign(blocksize, 0U);
+    sum.assign(blocksize, 0U);
+  }
+
+  inline void set(int k) { bit[k >> 5] |= 1U << (k & 31); }
+
+  inline void build() {
+    sum[0] = 0U;
+    for (int i = 1; i < blocksize; i++) {
+      sum[i] = sum[i - 1] + __builtin_popcount(bit[i - 1]);
     }
+  }
 
-    inline void set(int k) { bit[k >> 6] |= 1ULL << (k & 63ULL); }
+  inline bool access(unsigned k) {
+    return (bool((bit[k >> 5] >> (k & 31)) & 1));
+  }
 
-    inline void build() {
-        sum[0] = 0ULL;
-        for (int i = 1; i < blocksize; i++) {
-            sum[i] = sum[i - 1] + __builtin_popcountll(bit[i - 1]);
-        }
-    }
-
-    inline bool access(unsigned k) {
-        return (bool((bit[k >> 6] >> (k & 63)) & 1));
-    }
-
-    inline int rank(int k) {
-        return (sum[k >> 6] + __builtin_popcountll(bit[k >> 6] & ((1ULL << (k & 63)) - 1)));
-    }
-
-    inline void ft_set(const vector<T> &v) {
-        seg = fenwick_tree<T>(v.size());
-        for (int i = 0; i < v.size(); i++) {
-            if (v[i] != 0) {
-                seg.add(i, v[i]);
-            }
-        }
-    }
-
-    inline void val_add(unsigned pos, T x) {
-        seg.add(pos, x);
-    }
-
-    inline T val_get(unsigned pos) {
-        return seg.sum(pos, pos + 1);
-    }
-
-    inline T rank_sum(unsigned l, unsigned r) {
-        return seg.sum(l, r);
-    }
+  inline int rank(int k) {
+    return (sum[k >> 5] + __builtin_popcount(bit[k >> 5] & ((1U << (k & 31)) - 1)));
+  }
 };
 
-template <class T>
+template <class S, class T>
 class WaveletMatrix {
-   private:
-    unsigned n;
-    unsigned bitsize, logn;
-    vector<BitVector<T>> b;
-    vector<unsigned> zero;
-    vector<unsigned> index;
-    vector<T> cmp;
-    vector<T> px;
-    T MI, MA;
+ private:
+  unsigned n;
+  unsigned bitsize;
+  vector<BitVector> b;
+  vector<fenwick_tree<S>> fen;
+  vector<unsigned> zero;
+  vector<T> cmp;
+  T MI, MA;
 
-    inline unsigned compress(const T &x) {
-        return lower_bound(cmp.begin(), cmp.end(), x) - begin(cmp);
+  inline unsigned compress(const T &x) {
+    return lower_bound(cmp.begin(), cmp.end(), x) - begin(cmp);
+  }
+
+ public:
+  // コンストラクタ
+  WaveletMatrix() {}
+  WaveletMatrix(vector<T> v) {
+    MI = numeric_limits<T>::min();
+    MA = numeric_limits<T>::max();
+    n = v.size();
+    cmp = v;
+    sort(cmp.begin(), cmp.end());
+    cmp.erase(unique(cmp.begin(), cmp.end()), cmp.end());
+    vector<T> tmp(n);
+    vector<T> tmpc(n);
+    vector<T> compressed(n);
+    for (unsigned i = 0; i < n; i++) {
+      compressed[i] = distance(cmp.begin(), lower_bound(cmp.begin(), cmp.end(), v[i]));
     }
-
-   public:
-    // コンストラクタ
-    WaveletMatrix() {}
-    WaveletMatrix(vector<T> v) {
-        MI = numeric_limits<T>::min();
-        MA = numeric_limits<T>::max();
-        n = v.size();
-        cmp = v;
-        sort(cmp.begin(), cmp.end());
-        cmp.erase(unique(cmp.begin(), cmp.end()), cmp.end());
-        vector<T> tmp(n);
-        vector<T> tmpc(n);
-        vector<T> compressed(n);
-        for (unsigned i = 0; i < n; i++) {
-            compressed[i] = distance(cmp.begin(), lower_bound(cmp.begin(), cmp.end(), v[i]));
-        }
-        bitsize = bit_width(cmp.size());
-        b.resize(bitsize + 1);
-        zero.resize(bitsize, 0);
-        int cur = 0;
-        for (unsigned i = 0; i < bitsize; i++) {
-            b[i] = BitVector<T>(n + 1);
-            b[i].ft_set(v);
-            cur = 0;
-            for (unsigned j = 0; j < n; j++) {
-                if (compressed[j] & (1U << (bitsize - i - 1))) {
-                    b[i].set(j);
-                } else {
-                    zero[i]++;
-                    tmpc[cur] = compressed[j];
-                    tmp[cur] = v[j];
-                    cur++;
-                }
-            }
-            b[i].build();
-            for (int j = 0; j < n; j++) {
-                if (compressed[j] & (1U << (bitsize - i - 1))) {
-                    tmpc[cur] = compressed[j];
-                    tmp[cur] = v[j];
-                    cur++;
-                }
-            }
-            swap(tmpc, compressed);
-            swap(tmp, v);
-        }
-        b[bitsize] = BitVector<T>(n + 1);
-        b[bitsize].ft_set(v);
-    }
-
-    WaveletMatrix(vector<T> x, vector<T> y, vector<T> w) {
-        n = x.size();
-        px.resize(n);
-        index.resize(n);
-        vector<tuple<T, T, T, int>> v(n);
-        for (int i = 0; i < n; i++) {
-            v[i] = {x[i], y[i], w[i], i};
-        }
-        sort(v.begin(), v.end(), [](const tuple<T, T, T, int> &l, const tuple<T, T, T, int> &r) {
-            return std::get<0>(l) < std::get<0>(r);
-        });
-        for (int i = 0; i < n; i++) {
-            px[i] = std::get<0>(v[i]);
-            y[i] = std::get<1>(v[i]);
-            w[i] = std::get<2>(v[i]);
-            index[std::get<3>(v[i])] = i;
-        }
-        vector<unsigned> compressed(n);
-        cmp.resize(y.size());
-        cmp = y;
-        sort(cmp.begin(), cmp.end());
-        cmp.erase(unique(cmp.begin(), cmp.end()), cmp.end());
-        vector<unsigned> tmpc(n);
-        vector<T> tmp(n);
-        unsigned size_mx = y.size();
-        for (unsigned i = 0; i < n; i++) {
-            compressed[i] = distance(cmp.begin(), lower_bound(cmp.begin(), cmp.end(), y[i]));
-        }
-        bitsize = bit_width(cmp.size());
-        b.resize(bitsize + 1);
-        zero.resize(bitsize, 0);
-        int cur = 0;
-        for (unsigned i = 0; i < bitsize; i++) {
-            b[i] = BitVector<T>(n + 1);
-            b[i].ft_set(w);
-            cur = 0;
-            for (unsigned j = 0; j < n; j++) {
-                if (compressed[j] & (1U << (bitsize - i - 1))) {
-                    b[i].set(j);
-                } else {
-                    zero[i]++;
-                    tmpc[cur] = compressed[j];
-                    tmp[cur] = w[j];
-                    cur++;
-                }
-            }
-
-            b[i].build();
-            for (int j = 0; j < n; j++) {
-                if (compressed[j] & (1U << (bitsize - i - 1))) {
-                    tmpc[cur] = compressed[j];
-                    tmp[cur] = w[j];
-                    cur++;
-                }
-            }
-            swap(tmpc, compressed);
-            swap(tmp, w);
-        }
-
-        b[bitsize].ft_set(w);
-    }
-
-    // get v[k]
-    T access(unsigned k) {
-        unsigned res = 0;
-        unsigned cur = k;
-        for (unsigned i = 0; i < bitsize; i++) {
-            if (b[i].access(cur)) {
-                res |= (1U << (bitsize - i - 1));
-                cur = zero[i] + b[i].rank(cur);
-            } else {
-                cur -= b[i].rank(cur);
-            }
-        }
-        return cmp[res];
-    }
-
-    void set(int p, T x) {
-        unsigned cur = index[p];
-        T before = b[0].val_get(cur);
-        for (unsigned i = 0; i < bitsize; i++) {
-            b[i].val_add(cur, x - before);
-            if (b[i].access(cur)) {
-                cur = zero[i] + b[i].rank(cur);
-            } else {
-                cur -= b[i].rank(cur);
-            }
-        }
-        b[bitsize].val_add(cur, x - before);
-    }
-
-    // v[l,r) の中でk番目(1-origin)に小さい値を返す
-    T kth_smallest(unsigned l, unsigned r, unsigned k) {
-        unsigned res = 0;
-        for (unsigned i = 0; i < bitsize; i++) {
-            unsigned num1 = b[i].rank(r) - b[i].rank(l);
-            unsigned num0 = r - l - num1;
-            if (num0 < k) {
-                res |= (1ULL << (bitsize - i - 1));
-                l = zero[i] + b[i].rank(l);
-                r = zero[i] + b[i].rank(r);
-                k -= num0;
-            } else {
-                l -= b[i].rank(l);
-                r -= b[i].rank(r);
-            }
-        }
-        return cmp[res];
-    }
-
-    // v[l,r) の中でk番目(1-origin)に大きい値を返す
-    T kth_largest(unsigned l, unsigned r, unsigned k) {
-        return kth_smallest(l, r, r - l - k + 1);
-    }
-
-    // v[l,r) の中で[mink,maxk)に入る値の総積を返す
-    T range_sum(int vl, int vr, T mink, T maxk) {
-        int D = compress(mink);
-        int U = compress(maxk);
-        T res = 0;
-        auto dfs = [&](auto &rec, int d, int L, int R, int A, int B) -> void {
-            if (U <= A or B <= D) return;
-            if (D <= A and B <= U) {
-                res += b[d].rank_sum(L, R);
-                return;
-            }
-            if (d == bitsize) {
-                if (D <= A and A < U) {
-                    res += b[bitsize].rank_sum(L, R);
-                }
-                return;
-            }
-            int C = (A + B) >> 1;
-            int rank0_l = L - b[d].rank(L);
-            int rank0_r = R - b[d].rank(R);
-            int rank1_l = b[d].rank(L) + zero[d];
-            int rank1_r = b[d].rank(R) + zero[d];
-
-            rec(rec, d + 1, rank0_l, rank0_r, A, C);
-            rec(rec, d + 1, rank1_l, rank1_r, C, B);
-        };
-        dfs(dfs, 0, vl, vr, 0, 1 << bitsize);
-        return res;
-    }
-
-    // v[l,r)の中でvalを超えない最大の値を返す
-    T prev_value(unsigned l, unsigned r, T val) {
-        int num = range_freq(l, r, MI, val);
-        if (num == 0) {
-            return MA;
+    bitsize = bit_width(cmp.size());
+    b.resize(bitsize + 1);
+    fen.resize(bitsize + 1);
+    zero.resize(bitsize, 0);
+    int cur = 0;
+    for (unsigned i = 0; i < bitsize; i++) {
+      b[i] = BitVector(n + 1);
+      fen[i] = fenwick_tree<T>(n);
+      cur = 0;
+      for (unsigned j = 0; j < n; j++) {
+        fen[i].add(j, v[j]);
+        if (compressed[j] & (1U << (bitsize - i - 1))) {
+          b[i].set(j);
         } else {
-            return kth_smallest(l, r, num);
+          zero[i]++;
+          tmpc[cur] = compressed[j];
+          tmp[cur] = v[j];
+          cur++;
         }
+      }
+      b[i].build();
+      for (int j = 0; j < n; j++) {
+        if (compressed[j] & (1U << (bitsize - i - 1))) {
+          tmpc[cur] = compressed[j];
+          tmp[cur] = v[j];
+          cur++;
+        }
+      }
+      swap(tmpc, compressed);
+      swap(tmp, v);
     }
+    b[bitsize] = BitVector(n + 1);
+    fen[bitsize] = fenwick_tree<T>(n);
+    for (unsigned i = 0; i < n; i++) {
+      fen[bitsize].add(i, v[i]);
+    }
+  }
 
-    // v[l,r)の中でval以上の最小の値を返す
-    T next_value(unsigned l, unsigned r, T val) {
-        int num = range_freq(l, r, MI, val);
-        if (num == r - l) {
-            return MI;
+  WaveletMatrix(vector<T> v, vector<S> w) {
+    MI = numeric_limits<T>::min();
+    MA = numeric_limits<T>::max();
+    n = v.size();
+    cmp = v;
+    sort(cmp.begin(), cmp.end());
+    cmp.erase(unique(cmp.begin(), cmp.end()), cmp.end());
+    vector<S> tmp(n);
+    vector<T> tmpc(n);
+    vector<T> compressed(n);
+    for (unsigned i = 0; i < n; i++) {
+      compressed[i] = distance(cmp.begin(), lower_bound(cmp.begin(), cmp.end(), v[i]));
+    }
+    bitsize = bit_width(cmp.size());
+    b.resize(bitsize + 1);
+    fen.resize(bitsize + 1);
+    zero.resize(bitsize, 0);
+    int cur = 0;
+    for (unsigned i = 0; i < bitsize; i++) {
+      b[i] = BitVector(n + 1);
+      fen[i] = fenwick_tree<S>(n);
+      cur = 0;
+      for (unsigned j = 0; j < n; j++) {
+        fen[i].add(j, w[j]);
+        if (compressed[j] & (1U << (bitsize - i - 1))) {
+          b[i].set(j);
         } else {
-            return kth_smallest(l, r, num + 1);
+          zero[i]++;
+          tmpc[cur] = compressed[j];
+          tmp[cur] = w[j];
+          cur++;
         }
+      }
+      b[i].build();
+      for (int j = 0; j < n; j++) {
+        if (compressed[j] & (1U << (bitsize - i - 1))) {
+          tmpc[cur] = compressed[j];
+          tmp[cur] = w[j];
+          cur++;
+        }
+      }
+      swap(tmpc, compressed);
+      swap(tmp, w);
     }
+    b[bitsize] = BitVector(n + 1);
+    fen[bitsize] = fenwick_tree<S>(n);
+    for (unsigned i = 0; i < n; i++) {
+      fen[bitsize].add(i, w[i]);
+    }
+  }
 
-    // x座標が[l,r) かつy座標が[d,u) に入る点の総積を返す
-    T rectangle_sum(T l, T r, T d, T u) {
-        unsigned cl = distance(px.begin(), lower_bound(px.begin(), px.end(), l));
-        unsigned cr = distance(px.begin(), lower_bound(px.begin(), px.end(), r));
-        return range_sum(cl, cr, d, u);
+  void set(int p, S x) {
+    unsigned cur = p;
+    S before = fen[0].get(p);
+    for (unsigned i = 0; i < bitsize; i++) {
+      fen[i].add(cur, x - before);
+      if (b[i].access(cur)) {
+        cur = zero[i] + b[i].rank(cur);
+      } else {
+        cur -= b[i].rank(cur);
+      }
     }
+    fen[bitsize].add(cur, x - before);
+  }
+
+  void add(int p, S x) {
+    unsigned cur = p;
+    for (unsigned i = 0; i < bitsize; i++) {
+      fen[i].add(cur, x);
+      if (b[i].access(cur)) {
+        cur = zero[i] + b[i].rank(cur);
+      } else {
+        cur -= b[i].rank(cur);
+      }
+    }
+    fen[bitsize].add(cur, x);
+  }
+
+  S get(int p) {
+    return fen[0].get(p);
+  }
+
+  // v[l,r) の中で[mink,maxk)に入る値の総和を返す
+  S range_sum(int vl, int vr, T mink, T maxk) {
+    int D = compress(mink);
+    int U = compress(maxk);
+    S res = 0;
+    auto dfs = [&](auto &rec, int d, int L, int R, int A, int B) -> void {
+      if (U <= A or B <= D) return;
+      if (D <= A and B <= U) {
+        res += fen[d].sum(L, R);
+        return;
+      }
+      if (d == bitsize) {
+        if (D <= A and A < U) {
+          res += fen[bitsize].sum(L, R);
+        }
+        return;
+      }
+      int C = (A + B) >> 1;
+      int rank0_l = L - b[d].rank(L);
+      int rank0_r = R - b[d].rank(R);
+      int rank1_l = b[d].rank(L) + zero[d];
+      int rank1_r = b[d].rank(R) + zero[d];
+
+      rec(rec, d + 1, rank0_l, rank0_r, A, C);
+      rec(rec, d + 1, rank1_l, rank1_r, C, B);
+    };
+    dfs(dfs, 0, vl, vr, 0, 1 << bitsize);
+    return res;
+  }
 };
--- docs/WaveletMatrixBinaryIndexedTree.md
+++ docs/WaveletMatrixBinaryIndexedTree.md
@@ -5,7 +5,115 @@ documentation_of: //data-structure/wavelet-matrix/WaveletMatrixBinaryIndexedTree
 
 ## 概要
 
-todo
+Wavelet MatrixとBITを同時に持つことで、数列に対して高度な1点加算・区間和のクエリを処理できるようにしたデータ構造。
 
-## 計算量
-todo
+
+## コンストラクタ
+
+```cpp
+(1) WaveletMatrix<T, S> wm(vector<T> v)
+(2) WaveletMatrix<T, S> wm(vector<T> v, vector<S> w)
+```
+
+長さ `n = v.size()` の数列 `v` に対してWavelet Matrixを構築する。
+- (1) 数列 `v` からビットベクトルとBITを構築する。
+- (2) 数列 `v` からビットベクトル、数列 `w` からBITを構築する。
+
+
+
+### 制約
+
+- テンプレート引数 T: データの型。
+- テンプレート引数 S: データの型。群の代数構造の性質を満たす必要がある。
+- (1) のとき、TとSの型は同一
+- (2) のとき、vとwの数列のサイズは等しい
+
+### 計算量
+- $O(n\log^2{n})$
+
+
+
+## range_sum
+
+```cpp
+S wm.range_sum(int l, int r, T mink, T maxk)
+```
+
+(1)  $l\leq i \lt r$ かつ $mink \leq v_i \lt maxk$ を満たす $i$ に対して、BITの和を計算して返す。条件を満たすインデックスがひとつも存在しないときは `0` を返す。
+
+例として、`v = {3, 1, 4, 1, 5, 9}`ならば、 
+
+```cpp
+S wm.range_sum(1, 5, 2, 5) // 返り値は4
+S wm.range_sum(0, 4, 1, 4) // 返り値は5
+```
+
+のようになる。
+
+(2)  $l\leq i \lt r$ かつ $mink \leq v_i \lt maxk$ を満たす $i$ に対して、BITの和を計算して返す。条件を満たすインデックスがひとつも存在しないときは `0` を返す。
+
+例として、`v = {3, 1, 4, 1, 5, 9}` 、 `w = {2, 7, 1, 8, 2, 8}` ならば、 
+
+```cpp
+S wm.range_prod(1, 5, 2, 6) // 返り値は3
+S wm.range_prod(2, 6, 1, 5) // 返り値は9
+```
+
+のようになる。
+
+### 制約
+- $0 \leq l \lt r \leq n$
+- `std::numeric_limits<T>::min()` $\leq mink \leq maxk \leq$ `std::numeric_limits<T>::max()`
+
+
+### 計算量
+- $O(\log^2{n})$
+
+
+## get
+
+```cpp
+S wm.get(int p)
+```
+
+(1) (2)  BITの `p` 番目の値を返す。 
+
+
+### 制約
+- $0 \leq p \lt n$
+
+### 計算量
+- $O(\log{n})$
+
+
+## set
+
+```cpp
+S wm.set(int p, S x)
+```
+
+(1) (2)  BITの `p` 番目に `x` を代入する。`v[p]` の値は変更しない。
+
+
+### 制約
+- $0 \leq p \lt n$
+
+
+### 計算量
+- $O(\log^2{n})$
+
+## add
+
+```cpp
+S wm.add(int p, S x)
+```
+
+(1) (2)  BITの `p` 番目に `x` を加算する。`v[p]` の値は変更しない。
+
+
+### 制約
+- $0 \leq p \lt n$
+
+
+### 計算量
+- $O(\log^2{n})$
