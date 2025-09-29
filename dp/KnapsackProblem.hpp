077be05f61bac87ef86e6edb764010a125250bd7
lmorinn <trin123123m@gmail.com>
2025-09-30 00:05:33 +0900

    add knapsack problem

---
+++ dp/KnapsackProblem.hpp
@@ -0,0 +1,15 @@
+template <class T>
+vector<T> knapsack(const vector<int> &w, const vector<T> &v, const int W) {
+    int n = int(v.size());
+    T ng = numeric_limits<T>::min();
+    vector<vector<T>> dp(n + 1, vector(W + 2, ng));
+    dp[0][0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (dp[i][j] == ng) continue;
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+            dp[i + 1][min(j + w[i], W + 1)] = max(dp[i + 1][min(j + w[i], W + 1)], dp[i][j] + v[i]);
+        }
+    }
+    return {dp[n].begin(), dp[n].end() - 1};
+}
---
+++ verify/AizuOnlineJudge/dp/DPL1_B.test.cpp
@@ -0,0 +1,15 @@
+#include "../../../template/template.hpp"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/1/DPL_1_B"
+#include "../../../dp/KnapsackProblem.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, W;
+    in(n, W);
+    vector<int> v(n), w(n);
+    rep(i, n) in(v[i], w[i]);
+    vector<int> res = knapsack<int>(w, v, W);
+    int ans = 0;
+    for (auto x : res) chmax(ans, x);
+    out(ans);
+}
