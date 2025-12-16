613199912d493d4d6ecebbb5950f199af2c3e3f8
lmorinn <trin123123m@gmail.com>
2025-12-17 00:28:18 +0900

    add Monotonic Min Queue

---
+++ data-structure/others/MonotonicMinQueue.hpp
@@ -0,0 +1,30 @@
+template <class T>
+class MonotonicMinQueue {
+ private:
+  queue<T> que;
+  deque<T> deq;
+
+ public:
+  void push(const T& x) {
+    que.push(x);
+    while (deq.size() > 0 and deq.back() > x) deq.pop_back();
+    deq.push_back(x);
+  }
+
+  void pop() {
+    if (que.front() == deq.front()) deq.pop_front();
+    que.pop();
+  }
+
+  const T& front() const {
+    return que.front();
+  }
+
+  bool empty() const {
+    return que.empty();
+  }
+
+  const T& minimum() const {
+    return deq.front();
+  }
+};
---
+++ docs/MonotonicMinQueue.md
@@ -0,0 +1,11 @@
+---
+title: Monotonic Queue (Min Queue)
+documentation_of: //data-structure/others/MonotonicMinQueue.hpp
+---
+
+## 概要
+
+todo
+
+## 計算量
+todo
