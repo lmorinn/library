19cc716682695845f9937c9681c8301ae52fb226
lmorinn <trin123123m@gmail.com>
2026-03-26 14:52:32 +0900

    add sharp-p-subsetsum

---
+++ docs/Sharp_P_SubsetSum.md
@@ -0,0 +1,11 @@
+---
+title: $\#_p$ Subset Sum
+documentation_of: //math/enumerative-combinatorics/Sharp_P_SubsetSum.hpp
+---
+
+## 概要
+
+todo
+
+## 計算量
+todo
---
+++ math/enumerative-combinatorics/Sharp_P_SubsetSum.hpp
@@ -0,0 +1,21 @@
+#include "../fps/FormalPowerSeries.hpp"
+
+FPS sharp_p_subset_sum(const vector<long long>& s, long long t) {
+  int n = int(s.size());
+  FPS log_f(t + 1);
+  vector<int> freq(t + 1);
+  for (int i = 0; i < n; i++) {
+    assert(0 < s[i]);
+    if (s[i] <= t) freq[s[i]]++;
+  }
+  vector<atcoder::modint998244353> iv(t + 1);
+  iv[1] = 1;
+  for (int i = 2; i < t + 1; i++) iv[i] = iv[998244353 % i] * (-(998244353 / i));
+  for (long long k = 1; k < t + 1; k++) {
+    if (freq[k] == 0) continue;
+    for (long long i = 1; k * i < t + 1; i++) {
+      log_f[k * i] += (i % 2 ? 1 : -1) * iv[i] * freq[k];
+    }
+  }
+  return log_f.exp();
+}
