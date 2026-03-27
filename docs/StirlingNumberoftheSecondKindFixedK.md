10e9a091df3bda7461faa4c2a6debb46a07e3043
lmorinn <trin123123m@gmail.com>
2026-03-27 21:00:12 +0900

    add stirling2nd fixed k

---
+++ docs/StirlingNumberoftheSecondKindFixedK.md
@@ -0,0 +1,11 @@
+---
+title: Stirling Number of the Second Kind (Fixed K)
+documentation_of: //math/enumerative-combinatorics/StirlingNumberoftheSecondKindFixedK.hpp
+---
+
+## 概要
+
+todo
+
+## 計算量
+todo
---
+++ verify/LibraryChecker/math/enumerative-combinatorics/StirlingNumberoftheSecondKindFixedK.test.cpp
@@ -0,0 +1,20 @@
+#include "../../../../template/template.hpp"
+#define PROBLEM "https://judge.yosupo.jp/problem/stirling_number_of_the_second_kind_fixed_k"
+#include "../../../../atcoder/convolution.hpp"
+#include "../../../../atcoder/modint.hpp"
+using namespace atcoder;
+using mint = modint998244353;
+
+#include "../../../../math/enumerative-combinatorics/StirlingNumberoftheSecondKindFixedK.hpp"
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int n, k;
+  in(n, k);
+  FPS res = Stirling_number_2nd_fixed_k(n, k);
+  rep(i, k, n + 1) {
+    cout << res[i].val();
+    if (i != n) cout << " ";
+  }
+  out();
+}
