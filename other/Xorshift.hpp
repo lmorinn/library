9f9a2e8dbaa9fc2f8b0d8cc74c436d56fdd0a9dd
amesyu <amesyuo777@gmail.com>
2026-04-13 17:27:38 +0900

    clean

--- other/Xorshift.hpp
+++
@@ -1,9 +0,0 @@
-inline static unsigned long long seed = 1235;
-unsigned long long rand_gen(long long p) {
-  unsigned long long x = seed;
-  x ^= x << 13;
-  x ^= x >> 7;
-  x ^= x << 17;
-  seed = x;
-  return seed % p;
-}
